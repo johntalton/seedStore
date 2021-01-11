73d6e2fe8d5199f703d0a7dc283fce2a89a8b3a0
johntalton <johntalton@gmail.com>
2021-01-11 02:15:05 -0500

    load walk into image as desired and performant

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Seed Store
+
+An attempt at providing a simple example of loaing and rendering from JSON configuration in p5.
+
+- module style (but not typescript due to target requirments)
+- query parameter seed loaing
+- JSON kvp and configuation
+- ascyn walk calcuation
+- seedable random for the web (`Math.random` is a no go)
+
+This is inspiered by the work over at [Coding Traing : Random Whistle](https://github.com/CodingTrain/Random-Whistle)
--- src/seedStore.js
+++ src/seedStore.js
@@ -1,19 +1,13 @@
 
 export class SeedStore {
   static async fetchSeeds() {
-    const result = await fetch('src/seeds.json', {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
+    return fetch('src/seeds.json', {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
       }
     })
-
-    return result
-  }
-
-  static async fetchSeed(name) {
-
   }
 }
